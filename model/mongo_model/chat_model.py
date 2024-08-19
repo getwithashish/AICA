eece7b58da87d41d3d7a0be08e7ed8267a50b81d
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-08-19 15:57:52 +0530

    feat: Create model for storing chat messages in MongoDB

---
+++ model/mongo_model/chat_model.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import List
+from uuid import UUID, uuid4
+from beanie import Document
+from pydantic import Field
+
+from model.chat_message_model import ChatMessageModel
+
+
+class ChatModel(Document):
+    user_id: UUID
+    chat_id: UUID = Field(default_factory=uuid4)
+    chat_title: str
+    updated_at: datetime
+    messages: List[ChatMessageModel]
+
+    class Settings:
+        name = "chat_collection"
+        indexes = [
+            {"fields": ["user_id"], "unique": True},
+            {"fields": ["updated_at"]},
+            {"fields": ["user_id", "created_at"]},  # Compound index
+        ]
