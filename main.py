5d3baa12915f4c8c3803545fd0ee73888a7a3a47
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-08-27 10:30:53 +0530

    feat: Create FastAPI app

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 
+from client.mongo.mongo_client import MongoClient
 from controller import chat_controller
+from controller import exception_handler
 
 
-app = FastAPI()
+# TODO Use Logging
+
+app = FastAPI(
+    title="Chatbot Backend Server",
+    on_startup=[
+        MongoClient.init,
+    ],
+)
 app.include_router(
     chat_controller.chat_router,
     prefix="/v1",
     tags=["Image"],
 )
+
+exception_handler.include_exception_handler_in_app(app=app)
