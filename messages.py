81cc4ef089cba5fafcb04d10f0d0440d5a41a2d1
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-08-27 10:12:20 +0530

    feat: Create static messages

---
+++ messages.py
@@ -0,0 +1,14 @@
+# Chat Messages
+AI_INFERENCE_SUCCESSFUL = "AI Model has successfully completed the inference"
+CHAT_SAVE_SUCCESSFUL = "The chat has been successfully saved"
+#
+CHAT_SESSION_NOT_FOUND = "Chat Session with Chat ID {chat_id} is not found for the user"
+
+# Validation Messages
+VALIDATION_FAILED = "Data validation has failed"
+
+# Auth Messages
+USER_NOT_SPECIFIED = "The user has not been specified"
+
+# Http Messages
+METHOD_NOT_ALLOWED = "This HTTP Method is not allowed for this endpoint"
