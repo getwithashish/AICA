0852768dc38213c76a41f923ca3bebec66129446
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-08-27 14:28:41 +0530

    feat: Update OpenAI client

--- client/openai/openai_client.py
+++ client/openai/openai_client.py
@@ -9,6 +9,7 @@ from client.openai.config import (
     ASSISTANT_ROLE_NAME,
     MODEL_NAME,
     OPENAI_API_BASE,
+    OPENAI_KEY,
     USER_ROLE_NAME,
 )
 from model.chat_message_model import ChatMessageRoleEnum
@@ -19,20 +20,22 @@ class OpenAIClient(AIClient):
 
     def __init__(self, model_name=MODEL_NAME):
         self.model_name = model_name
-        self.client = AsyncOpenAI(base_url=OPENAI_API_BASE, api_key="")
+        self.client = AsyncOpenAI(base_url=OPENAI_API_BASE, api_key=OPENAI_KEY)
 
     async def infer_from_text(self, messages: List) -> Choice:
-        class Dummy_Dict_Data(BaseModel):
-            content: str = "Hello, How are you?"
+        # class Dummy_Dict_Data(BaseModel):
+        #     content: str = "Hello, How are you?"
 
-        class Dummy_Dict(BaseModel):
-            message: Dummy_Dict_Data = Dummy_Dict_Data()
+        # class Dummy_Dict(BaseModel):
+        #     message: Dummy_Dict_Data = Dummy_Dict_Data()
 
-        ai_dummy_value = Dummy_Dict()
-        return ai_dummy_value.model_dump()
+        # ai_dummy_value = Dummy_Dict()
+        # return ai_dummy_value.model_dump()
         chat_completion = await self.client.chat.completions.create(
             messages=messages,
             model=f"{self.model_name}",
+            temperature=0.1,
+            logprobs=True,
         )
         return chat_completion.choices[0]
 
@@ -53,11 +56,12 @@ class OpenAIClient(AIClient):
 
     @staticmethod
     def format_ai_response(response: Choice) -> str:
-        return response["message"]["content"]
+        # return response["message"]["content"]
+        return response.message.content
 
     @staticmethod
     def calculate_ai_response_confidence(response: Choice) -> float:
-        return 70.0
+        # return 70.0
         logprobs: List[float] = []
         token_logprobs = response.logprobs.content
         for token_logprob in token_logprobs:
@@ -67,5 +71,6 @@ class OpenAIClient(AIClient):
         probability = np.exp(total_logprob)
         max_length = 100
         confidence_score = probability ** (1 / max_length)
+        confidence_score_percentage = confidence_score * 100
 
-        return confidence_score
+        return confidence_score_percentage
