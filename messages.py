3262f2ccbfd362b4e73e98b7ec41df05c44b85fc
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-10-08 15:27:51 +0530

    feat: Update messages.py

--- messages.py
+++ messages.py
@@ -1,6 +1,7 @@
 # Chat Messages
-AI_INFERENCE_SUCCESSFUL = "AI Model has successfully completed the inference"
+AI_INFERENCE_SUCCESSFUL = "Chat Request successfully completed"
 CHAT_SAVE_SUCCESSFUL = "The chat has been successfully saved"
+CHAT_LIST_RETRIEVED = "The chat list of the user has been successfully retrieved"
 #
 CHAT_SESSION_NOT_FOUND = "Chat Session with Chat ID {chat_id} is not found for the user"
 
@@ -9,6 +10,7 @@ VALIDATION_FAILED = "Data validation has failed"
 
 # Auth Messages
 USER_NOT_SPECIFIED = "The user has not been specified"
+USER_DOES_NOT_EXIST = "The specified user does not exist"
 
 # Http Messages
 METHOD_NOT_ALLOWED = "This HTTP Method is not allowed for this endpoint"
