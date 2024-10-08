f4a0c6274fd78bb406560d6b76e465b9a1995b73
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-10-08 15:26:53 +0530

    feat: Generate title of the chat asynchronously

--- service/chat_service.py
+++ service/chat_service.py
@@ -1,9 +1,13 @@
+import asyncio
 from datetime import datetime
 from typing import List, Optional
 from uuid import UUID
 
 from client.ai_client import AIClient
-from exceptions import ChatSessionNotFoundException
+from client.openai.config import TITLE_PROMPT
+from exceptions import ChatSessionNotFoundException, UnauthorizedUserException
+from messages import USER_DOES_NOT_EXIST
+from model.chat_list_response_model import ChatListResponseModel
 from model.chat_message_model import ChatMessageModel
 from model.chat_request_model import ChatRequestDataModel, ChatRequestRoleEnum
 from model.chat_response_model import ChatResponseModel
@@ -33,6 +37,16 @@ class ChatService:
             chat_repo=chat_repo, user_repo=user_repo
         )
 
+        # Convert Chat Request Message to Chat Message
+        for chat_request_message in chat_request_messages:
+            current_timestamp = datetime.now()
+            chat_message_model = ChatMessageModel(
+                role=chat_request_message.role,
+                content=chat_request_message.content,
+                timestamp=current_timestamp,
+            )
+            new_messages.append(chat_message_model)
+
         chat = await chat_history_service.get_chat_history(
             user_id=user_id, chat_id=chat_id
         )
@@ -44,23 +58,14 @@ class ChatService:
             chat = ChatModel(
                 user_id=user_id, chat_title=None, updated_at=None, messages=None
             )
-            # TODO Generate Chat Title in background
-            chat_title_response = await ChatService.initiate_inference(
-                messages=new_messages, ai_client=AIClient, generate_chat_title=True
+            # Generates Chat Title in Background
+            chat_title_task = asyncio.create_task(
+                ChatService.initiate_inference(
+                    messages=new_messages, ai_client=ai_client, generate_chat_title=True
+                )
             )
-            chat_title = chat_title_response.content
 
         chat_history_messages = chat.messages if chat.messages else []
-
-        # Convert Chat Request Message to Chat Message
-        for chat_request_message in chat_request_messages:
-            current_timestamp = datetime.now()
-            chat_message_model = ChatMessageModel(
-                role=chat_request_message.role,
-                content=chat_request_message.content,
-                timestamp=current_timestamp,
-            )
-            new_messages.append(chat_message_model)
 
         # Infer using AI Client if the new message author is a human
         if any(
@@ -78,6 +83,10 @@ class ChatService:
             chat_response.confidence = ai_chat_response.confidence
 
         latest_message_timestamp = new_messages[-1].timestamp
+
+        if not chat_exists:
+            chat_title_response = await chat_title_task
+            chat_title = chat_title_response.content
 
         # Populate the Chat Response Model for giving back to user
         chat_response.timestamp = latest_message_timestamp
@@ -97,16 +106,18 @@ class ChatService:
     async def initiate_inference(
         messages: List, ai_client: AIClient, generate_chat_title: bool = False
     ) -> ChatMessageModel:
+        chat_messages = list(messages)
         if generate_chat_title:
-            ai_chat_response = await ChatService.generate_chat_title(
-                messages=messages, ai_client=AIClient
+            title_generator_message = ChatRequestDataModel(
+                role=ChatRequestRoleEnum.human,
+                content=TITLE_PROMPT,
             )
-        else:
-            formatted_prompt_messages = ai_client.format_chat_message(messages=messages)
-            # TODO Can check here whether required inference is text, image, audio or video
-            ai_chat_response = await ChatService.infer_from_text(
-                messages=formatted_prompt_messages, ai_client=ai_client
-            )
+            chat_messages.append(title_generator_message)
+
+        formatted_prompt_messages = ai_client.format_chat_message(messages=chat_messages)
+        ai_chat_response = await ChatService.infer_from_text(
+            messages=formatted_prompt_messages, ai_client=ai_client
+        )
         return ai_chat_response
 
     @staticmethod
